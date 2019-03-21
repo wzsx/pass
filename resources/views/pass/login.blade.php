eff9db12d2c2fdd21de1bb3699210c7390c55ddf
wzsx <17630229059@163.com>
2019-03-21 13:41:11 +0800

    提交

---
+++ resources/views/pass/login.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 王晨琦
+ * Date: 2019/3/21
+ * Time: 12:00
+ */
---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+//用户注册
+Route::get('/user/reg','User\UserController@reg');
+Route::post('/user/reg','User\UserController@doReg');
+
+Route::get('/user/login','User\UserController@login');           //用户登录
+Route::post('/user/login','User\UserController@doLogin');        //用户登录
+Route::get('/user/center','User\UserController@center');      //个人中心
