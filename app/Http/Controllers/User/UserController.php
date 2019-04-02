6e1e3d1c96e6f5b17b4377f118b2d8fa300743ad
wzsx <17630229059@163.com>
2019-04-02 10:36:53 +0800

    222

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -16,44 +16,46 @@ class UserController extends Controller
         return view('user.login', $data);
     }
 
-    public function loginweb(Request $request){
+    public function loginweb(Request $request)
+    {
         //echo '<pre>';print_r($_POST);echo '</pre>';
         // echo __METHOD__;
         // echo '<pre>';print_r($_POST);echo '</pre>';
-        $pass =$request->input('pass');
-        $root=$request->input('email');
-        $r=$request->input('redirect')?? env('SHOP_URL');
+        $pass = $request->input('pass');
+        $root = $request->input('email');
+        $r = $request->input('redirect') ?? env('SHOP_URL');
         //var_dump($r);die;
-        $id2 = UserModel::where(['email'=>$root])->first();
+        $id2 = UserModel::where(['email' => $root])->first();
         //var_dump($id2);
-        if($id2){
-            if(password_verify($pass,$id2->pass)){
-                $token = substr(md5(time().mt_rand(1,99999)),10,10);
-                setcookie('token',$token,time()+86400,'/','52xiuge.com',false,true);
-                setcookie('email',$id2->email,time()+86400,'/','52xiuge.com',false,true);
-                setcookie('uid',$id2->uid,time()+86400,'/','52xiuge.com',false,true);
+        if ($id2) {
+            if (password_verify($pass, $id2->pass)) {
+                $token = substr(md5(time() . mt_rand(1, 99999)), 10, 10);
+                setcookie('token', $token, time() + 86400, '/', '52xiuge.com', false, true);
+                setcookie('email', $id2->email, time() + 86400, '/', '52xiuge.com', false, true);
+                setcookie('uid', $id2->uid, time() + 86400, '/', '52xiuge.com', false, true);
 //                $redis_key_web='str:u:web:'.$id2->uid;
 //                Redis::set($redis_key_web,$token);
 //                Redis::expire($redis_key_web,86400);
                 // echo $redis_key_web;die;
-                $redis_key_web_token='str:u:token:'.$id2->uid;
+                $redis_key_web_token = 'str:u:token:' . $id2->uid;
                 Redis::del($redis_key_web_token);
-                Redis::hSet($redis_key_web_token,'web',$token);
+                Redis::hSet($redis_key_web_token, 'web', $token);
                 header("Refresh:3;$r");
                 echo '登录成功';
             } else {
                 die('密码或用户名错误');
 
             }
-        }else{
+        } else {
             die('用户不存在');
         }
 
     }
+
     public function login()
     {
-        $user_account = $_POST['nick_name'];
-        $user_pwd = $_POST['pass'];
+        $user_account = $_POST['user_name'];
+        $user_pwd = $_POST['user_pwd'];
         if (empty($user_account)) {
             $res_data = [
                 'errcode' => '5010',
@@ -71,32 +73,32 @@ class UserController extends Controller
         if (is_numeric($user_account) || strlen($user_account) == 11) {
             $user_where = [
                 'user_tel' => $user_account,
-                'pass' => $user_pwd
+                'user_pwd' => $user_pwd
             ];
         } elseif (substr_count($user_account, '@') != 0) {
             $user_where = [
-                'email' => $user_account,
-                'pass' => $user_pwd
+                'user_email' => $user_account,
+                'user_pwd' => $user_pwd
             ];
         } else {
             $user_where = [
-                'nick_name' => $user_account,
-                'pass' => $user_pwd
+                'user_name' => $user_account,
+                'user_pwd' => $user_pwd
             ];
         }
 
-        $user_data = UserModel::where($user_where)->first();
-        $ktoken = 'token:u:' . $user_data['uid'];
+        $user_data = UsersModel::where($user_where)->first();
+        $ktoken = 'token:u:' . $user_data['user_id'];
         $token = $token = str_random(32);
         Redis::hSet($ktoken, 'app:token', $token);
-        Redis::expire($ktoken, 60 * 2 );
+        Redis::expire($ktoken, 3600 * 24 * 3);
         if ($user_data) {
             $res_data = [
                 'errcode' => 0,
                 'msg' => '登陆成功',
                 'token' => $token,
-                'uid' => $user_data['uid'],
-                'nick_name' => $user_data['nick_name'],
+                'user_id' => $user_data['user_id'],
+                'user_name' => $user_data['user_name'],
             ];
         } else {
             $res_data = [
---
+++ app/Model/UsersModel.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UsersModel extends Model
+{
+    //
+    public $table = 'api_users';
+    public $timestamps = false;
+    protected $primaryKey = 'id';
+}
